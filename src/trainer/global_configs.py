b0c7a90c52f29ad6ccaedefc121046b1080d01be
michal.wojdylak <michal.wojdylak@wundermanthompson.com>
2023-08-25 15:28:44 +0200

    fix

--- src/trainer/global_configs.py
+++ src/trainer/global_configs.py
@@ -1,7 +1,7 @@
-from pathlib import Path
+import os
 
-ROOT = f"{str(Path(__file__).parent)}/download"
-KMEANS_WEIGHT_ROOT = f"{str(Path(__file__).parent.parent.parent.parent)}/download/clustering_weights"
-DATASET_DIR = f"{ROOT}/datasets"
-FID_WEIGHT_DIR = f"{ROOT}/fid_weights/FIDNetV3"
-JOB_DIR = f"{ROOT}/pretrained_weights"
+ROOT = os.path.join("./", "download")
+KMEANS_WEIGHT_ROOT = os.path.join(ROOT, "clustering_weights")
+DATASET_DIR = os.path.join(ROOT, "datasets")
+FID_WEIGHT_DIR = os.path.join(ROOT, "fid_weights", "FIDNetV3")
+JOB_DIR = os.path.join(ROOT, "pretrained_weights")
